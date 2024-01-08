58ee8104bac860f0443b2ec68be26918b5866432
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-01-08 13:47:56 -0400

    refactor: :fire: Remove unused imports, constants and findEocdrSignature function from liber_epub.dart

--- lib/liber_epub.dart
+++ lib/liber_epub.dart
@@ -1,12 +1,8 @@
 library liber_epub;
 
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:liber_epub/zip_local_file_header.dart';
-
-const EOCD_SIGNATURE = 0x06054b50;
-const ZIP_SIGNATURE = 0x02014b50;
 
 class Reader {
   static Future decompressEpub(String path, String password) async {
@@ -21,17 +17,4 @@ class Reader {
       print(header.fileName);
     }
   }
-}
-
-ByteBuffer findEocdrSignature(Uint8List bytes) {
-  for (int i = bytes.lengthInBytes - 4; i >= 0; i--) {
-    final buffer = bytes.buffer;
-
-    final potentialSignature = buffer.asByteData().getUint32(i, Endian.little);
-    if (potentialSignature == EOCD_SIGNATURE) {
-      return bytes.sublist(i).buffer;
-    }
-  }
-
-  throw Exception('EOCDR not found in zip file');
 }
