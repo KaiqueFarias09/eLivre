62fa504dadde82cfc7c7d44d1f882f0826dfaa11
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-01-10 18:30:52 -0400

    test: :white_check_mark: Update readBook tests

--- test/liber_epub_test.dart
+++ test/liber_epub_test.dart
@@ -1,34 +1,51 @@
-import 'package:liber_epub/liber_ebooks.dart';
+import 'dart:io';
+
+import 'package:liber_epub/features/epub/utils/read_book.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Reader', () {
-    test('should throw exception when path is empty', () async {
-      const path = '';
-      try {
-        await Reader().decompressEpub(path);
-        fail('Exception not thrown');
-      } catch (exception) {
-        expect(exception, isA<Exception>());
-        expect(exception.toString(), 'Exception: Path cannot be empty');
-      }
-    });
+  group(
+    'Reader',
+    () {
+      test('should throw exception when path is empty', () async {
+        const path = '';
+        try {
+          await readBook(path);
+          fail('Exception not thrown');
+        } catch (exception) {
+          expect(exception, isA<Exception>());
+          expect(exception.toString(), 'Exception: Path cannot be empty');
+        }
+      });
 
-    test('should throw exception when file does not exist', () async {
-      const path = '/path/to/nonexistent/file.epub';
-      try {
-        await Reader().decompressEpub(path);
-        fail('Exception not thrown');
-      } catch (exception) {
-        expect(exception, isA<Exception>());
-        expect(exception.toString(), 'Exception: No such file or directory');
-      }
-    });
+      test('should throw exception when file does not exist', () async {
+        const path = '/path/to/nonexistent/file.epub';
+        try {
+          await readBook(path);
+          fail('Exception not thrown');
+        } catch (exception) {
+          expect(exception, isA<Exception>());
+          expect(exception.toString(), 'Exception: No such file or directory');
+        }
+      });
 
-    test('should succeed', () async {
-      const path =
-          'test/resources/Harry_Potter_and_the_Chamber_of_Secrets_Harry_Potter_2.epub';
-      await Reader().decompressEpub(path);
-    });
-  });
+      test(
+        'should succeed',
+        () async {
+          try {
+            final directory = Directory('test/resources');
+            final files = directory.listSync();
+
+            for (final file in files) {
+              if (file is File && file.path.endsWith('.epub')) {
+                await readBook(file.path);
+              }
+            }
+          } catch (exception) {
+            fail('Exception thrown');
+          }
+        },
+      );
+    },
+  );
 }
