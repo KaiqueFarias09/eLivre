63d67cc3229a1864a206138f410ebb9eca80a728
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-01-10 18:31:35 -0400

    feat: :truck: Add exports

---
+++ lib/features/epub/entities/entities.dart
@@ -0,0 +1,10 @@
+export 'book/book.dart';
+export 'book/files.dart';
+export 'file/binary_file.dart';
+export 'file/epub_file.dart';
+export 'file/text_file.dart';
+export 'navigation/nav_point.dart';
+export 'navigation/navigation.dart';
+export 'package/epub_2_package.dart';
+export 'package/epub_3_package.dart';
+export 'package/epub_package.dart';
