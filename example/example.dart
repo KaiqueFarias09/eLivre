593036721c4111ec23ffcc1a78e272d3f0a4a66b
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-01-11 20:58:44 -0400

    docs: :memo: Add example.dart file with code to read book metadata

---
+++ example/example.dart
@@ -0,0 +1,9 @@
+import 'package:e_livre/e_livre.dart' as livre;
+
+void main() async {
+  final book = await livre.readBook('test/resources/linear-algebra.epub');
+
+  print(book.package.metadata.title);
+  print(book.package.metadata.creator);
+  print(book.package.metadata.language);
+}
