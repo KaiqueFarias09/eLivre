95e951e8e387f3f6317e6068b69b0a7102a56eb5
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-02-06 14:51:48 -0400

    feat: :sparkles: Add new example usage

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,30 +1,3 @@
-# eLivre
-
-This is a Dart package for handling ebook files in EPUB 2.0 and 3.0 formats.
-
-## Features
-
--   Read EPUB 2.0 and EPUB 3.0 files
--   Navigate through the book structure
--   Extract book metadata
--   Extract book files
-
-## Getting Started
-
-To use this package, add `e_livre` as a dependency in your `pubspec.yaml` file.
-
-```dartx
-dependencies:
-  e_livre: ^2.0.0
-```
-
-Then, run `flutter pub get` in your terminal.
-
-## Usage
-
-Here's a basic example of how to use the package:
-
-```dart
 import 'package:e_livre/e_livre.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -197,4 +170,3 @@ class Reader extends StatelessWidget {
     );
   }
 }
-```
